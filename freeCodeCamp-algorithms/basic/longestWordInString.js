3f2ac0545d44af44011cb626b1096d96d6c3b078
aboseu <bexis17@gmail.com>
2017-06-20 14:02:48 +0000

    add solution for find the longest word in a string problem

---
+++ freeCodeCamp-algorithms/basic/longestWordInString.js
@@ -0,0 +1,25 @@
+/*
+Find the Longest Word in a String
+
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+*/
+
+//my solution
+
+function findLongestWord(str) {
+ var string= str.split(" ");
+  var maxString = string[0].length;
+  for (var i=0; i < string.length; i++){
+    
+    if(string[i].length > maxString){
+      maxString = string[i].length;
+    }
+    
+  }
+  return maxString;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
