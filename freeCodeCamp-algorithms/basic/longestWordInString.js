33058a98719b06afb247d6fe25584de86c057792
aboseu <bexis17@gmail.com>
2017-06-20 14:20:02 +0000

    add solution for largest  number in array and add comments to longest word in a string

--- freeCodeCamp-algorithms/basic/longestWordInString.js
+++ freeCodeCamp-algorithms/basic/longestWordInString.js
@@ -9,16 +9,15 @@ Your response should be a number.
 //my solution
 
 function findLongestWord(str) {
- var string= str.split(" ");
-  var maxString = string[0].length;
-  for (var i=0; i < string.length; i++){
-    
-    if(string[i].length > maxString){
-      maxString = string[i].length;
+    var string= str.split(" ");
+    var maxString = string[0].length;
+   
+    for (var i=0; i < string.length; i++){ //find string with the longest length
+        if(string[i].length > maxString){
+            maxString = string[i].length;
+        }
     }
-    
-  }
-  return maxString;
+    return maxString;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
