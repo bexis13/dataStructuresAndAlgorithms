82c644fe106896c85869c5904cfc08e1149fd7d2
aboseu <bexis17@gmail.com>
2017-06-29 10:08:16 +0000

    add shorter solution for repeat a string algo challenge

--- freeCodeCamp-algorithms/basic/repeatAString.js
+++ freeCodeCamp-algorithms/basic/repeatAString.js
@@ -18,5 +18,12 @@ function repeatStringNumTimes(str, num) {
    return result;
   }
 }
-
+ //or shorter solution using built in function
+function repeatStringNumTimes(str, num){
+    if(num <= 0){return "";}
+    return str.repeat(num);
+ }
+ 
+ 
+ 
 repeatStringNumTimes("abc", 3);
