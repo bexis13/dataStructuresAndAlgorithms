2d32867650a6caf97548fec0881c5e283407109b
aboseu <bexis17@gmail.com>
2017-06-29 09:58:48 +0000

    add solution to basic algo repeat-a-string

---
+++ freeCodeCamp-algorithms/basic/repeatAString.js
@@ -0,0 +1,22 @@
+/*
+Repeat a string repeat a string 
+
+Repeat a given string (first argument) num times (second argument). Return an 
+empty string if num is not a positive number.
+*/
+
+
+function repeatStringNumTimes(str, num) {
+  var result="";
+  if (num <= 0){
+    return "";
+  }
+   else{
+     for (var i =0;i<num; i++){
+       result += str;
+     } 
+   return result;
+  }
+}
+
+repeatStringNumTimes("abc", 3);
