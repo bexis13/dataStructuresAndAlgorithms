920a1cc474138176531ac8487528c09ac281936a
Abose Ukhun <bexis17@gmail.com>
2017-09-16 08:52:35 +0000

    add docs for my solution

--- lynda-algorithms/bubbleSort.js
+++ lynda-algorithms/bubbleSort.js
@@ -1,6 +1,11 @@
 //bubble sort
 /*
+In bubble sort, we keep iterating while something was swapped in
+the previous inner-loop iteration. By swapped I mean, in the 
+inner loop iteration, we check each number if the number proceeding 
+it is greater than itself, if so we swap them.
 
+The big O on bubble sort is O(N^2). Not efficient.
 */
 
 function bubbleSort(arr){
