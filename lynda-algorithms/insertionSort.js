56d713372e3605ca7e27469b12a3a984d6296668
Abose Ukhun <bexis17@gmail.com>
2017-10-16 07:12:43 +0000

    do some part of the insertion sort algo

--- lynda-algorithms/insertionSort.js
+++ lynda-algorithms/insertionSort.js
@@ -1,10 +1,19 @@
 //insertion sort
 
 function insertionSort(arr){
-   
-   return arr; 
+    for( var i = 1; i < arr.length; i++ ){
+        
+        for( var j = i-1; j >= 0 && arr[] > arr; j--){
+            if()
+        }
+    }
+    return arr; 
 }
 
 //test
 var array =  [10,5,3,8,2,6,4,7,9,1];
 console.log(insertionSort(array)); // [1,2,3,4,5,6,7,8,9,10]
+
+
+  
+ 
