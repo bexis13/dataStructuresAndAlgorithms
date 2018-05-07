37336bb52699c29ac145515e59ac49e3750aa13a
Abose Ukhun <bexis17@gmail.com>
2018-05-07 09:56:49 +0100

    add solution for whereArtThou problem

--- freeCodeCamp-algorithms/intermediate/whereArtThou.js
+++ freeCodeCamp-algorithms/intermediate/whereArtThou.js
@@ -15,7 +15,21 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
- 
+for( var i = 0; i <collection.length; i++){
+    var found = false;
+    for (var j in source){
+        if( collection[i][j] && collection[i][j] === source[j]){
+            found = true;
+            continue;
+        }else{
+            found = false;
+            break;
+        }
+    }
+    if(found){
+        arr.push(collection[i]);
+    }
+  }
   // Only change code above this line
   return arr;
 }
