629d6a2058aedad9adce80f71c6478e6f9514cb4
Abose Ukhun <bexis17@gmail.com>
2018-05-08 09:59:38 +0100

    add my solution for fcc pigLatin challenge problem

--- freeCodeCamp-algorithms/intermediate/pigLatin.js
+++ freeCodeCamp-algorithms/intermediate/pigLatin.js
@@ -7,9 +7,27 @@
 *Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 */
 
+
 function translatePigLatin(str) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  if( vowels.indexOf( str[0]) !== -1){
+     str = str + "way";
+     return str;
+  }
+  var consonants = "";
+  str = str.split('');
+  for( var i = 0; i < str.length; i++){
+    if( vowels.indexOf( str[0]) !== -1){
+      break;
+    }
+    consonants += str[0];
+    str.shift();
+    
+  }
+  str = str.join('');
+  str = str + consonants + "ay";
   return str;
+ 
 }
 
-translatePigLatin("california"); //should return "aliforniacay"
-
+translatePigLatin("consonant"); //should return onsonantcay
