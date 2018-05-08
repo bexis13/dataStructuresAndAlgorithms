457bd63660da42727069d6d02a51e2be9bfc8479
Abose Ukhun <bexis17@gmail.com>
2018-05-08 22:44:34 +0100

    add my solution to fcc missing-Letter problem challenge

--- freeCodeCamp-algorithms/intermediate/missingLetter.js
+++ freeCodeCamp-algorithms/intermediate/missingLetter.js
@@ -7,7 +7,30 @@
 */
 
 function fearNotLetter(str) {
-  return str;
+    var currentCharacterCode;
+    var nextCharacterCode;
+    var range;
+    var missingString = "";
+    //
+    for(var i = 0; i < str.length; i++){
+        currentCharacterCode = str.charCodeAt(i);
+        nextCharacterCode = str.charCodeAt(i+1);
+        //
+        if( i === str.length-1){
+        return undefined;
+        }
+        //
+        if( currentCharacterCode+1 !== nextCharacterCode ){
+        range = nextCharacterCode - currentCharacterCode -1;
+        console.log(range);
+        //
+        for(var j = 0; j < range; j++){
+            currentCharacterCode += 1;
+            missingString += String.fromCharCode(currentCharacterCode);
+        }
+        return missingString;
+        }
+    }
 }
 
 fearNotLetter("abce"); //should return "d"
