5b86bbb88e8ad486c2d35286acfe95a17e577e5d
Abose Ukhun <bexis17@gmail.com>
2018-05-08 09:50:42 +0100

    add pigLatin challenge problem, method stub and test

---
+++ freeCodeCamp-algorithms/intermediate/pigLatin.js
@@ -0,0 +1,15 @@
+/**
+*Translate the provided string to pig latin.
+*( https://en.wikipedia.org/wiki/Pig_Latin) Pig Latin  takes the first consonant (or consonant cluster) of an English word, 
+*moves it to the end of the word and suffixes an "ay".
+*If a word begins with a vowel you just add "way" to the end.
+*Input strings are guaranteed to be English words in all lowercase.
+*Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+*/
+
+function translatePigLatin(str) {
+  return str;
+}
+
+translatePigLatin("california"); //should return "aliforniacay"
+
