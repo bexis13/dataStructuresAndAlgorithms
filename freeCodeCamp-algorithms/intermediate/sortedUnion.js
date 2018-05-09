cd2194525cd9734482f55d90c991e896765a4677
Abose Ukhun <bexis17@gmail.com>
2018-05-09 15:49:22 +0100

    add my solution for fcc sortedUnion challenge

--- freeCodeCamp-algorithms/intermediate/sortedUnion.js
+++ freeCodeCamp-algorithms/intermediate/sortedUnion.js
@@ -14,6 +14,13 @@
 */
 
 function uniteUnique(arr) {
+  for( var i = 1; i < arguments.length; i++){
+    for( var j = 0; j< arguments[i].length; j++){
+      if( arr.indexOf(arguments[i][j]) === -1 ){
+        arr.push(arguments[i][j]);
+      }
+    }
+  }
   return arr;
 }
 
