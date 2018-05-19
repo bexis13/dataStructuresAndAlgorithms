57044d2d5d65b9a49c06a2e480906642f50e55f5
Abose Ukhun <bexis17@gmail.com>
2018-05-19 15:57:10 +0100

    add for firecode delete-a-lists-tail-node question: the question statement, method stub and example test cases

---
+++ firecode/deleteAListsTailNode.java
@@ -0,0 +1,19 @@
+ java.util.*;
+ java.util.streams.*; 
+
+ /**
+ Given a singly-linked list, write a method to delete
+ its last node and return the head.
+
+Example:
+
+1->2->3->4 ==> 1->2->3
+1->2 ==> 1
+1 ==> null
+null ==> null
+  */
+
+public ListNode deleteAtTail(ListNode head) {
+
+    return head;
+}
